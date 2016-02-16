254de69bc45393fda925a2cd6928ee34e753ee33
RaulJimenezOrtega <hhkaos@gmail.com>
2016-02-16 13:29:14 +0100

    Adding skeleton

---
+++ ejercicios/js1/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Almacenar en una variable el resultado de sumar 1 y 2
+var a = 1 + 2;
+console.log(a);
+
+// Almacenar en una variable el resultado de dividir 6 entre 2
+
+
+// Almacenar en una variable el precio de un artículo de 20€ aplicándole el 21% de IVA .
+
+
+// Definir una variable con el valor 4 y utilizar el operador (```++```) para incrementar en uno su valor.
+
+
+// Definir una variable que almacene la concatenación de dos cadenas de texto.
+
+
+// Definir una variable ```price``` con el valor ```19.99```y aplicar la abreviación ```/=``` para dividirlo entre ```1.21``` para obtener el precio sin IVA.
+
+
+// Asignar a dos variables valores booleanos y hacer al menos una operación combinando un operador lógico: **AND** (```&&```) o **OR** (```||```)
+
+
+// Realizar 4 expresiones que utilicen operadores relacionales (```<```, ```==```, ```!=``` y ```===```) y almacenen los valores en tres variables distintas.
